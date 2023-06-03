f391f0d35211b23d35481452db8ff2644c857b90
Sandip Rode <16761010+Sandip1447@users.noreply.github.com>
2023-06-03 15:55:39 +0530

    Loading Skeleton

--- src/media/components/UsersList.js
+++ src/media/components/UsersList.js
@@ -1,32 +1,37 @@
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchUsers} from "../store/MediaStore";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
-
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const {isLoading, data, error} = useSelector((state) => {
-        return state.users
-    })
+        return state.users;
+    });
 
     useEffect(() => {
-        dispatch(fetchUsers())
-    }, [dispatch])
+        dispatch(fetchUsers());
+    }, [dispatch]);
 
-    if (isLoading){
-        return <div>Loading...</div>
+    if (isLoading) {
+        return <Skeleton times={6} className="h-10 w-full"/>;
     }
 
-    if (error){
-        return <div>Error fetching data...</div>
+    if (error) {
+        return <div>Error fetching data...</div>;
     }
 
-    return (
-        <div>
-            Users List {data.length}
-        </div>
-    )
+    const renderedUsers = data.map((user) => {
+        return (
+            <div key={user.id} className="mb-2 border rounded">
+                <div className="flex p-2 justify-between items-center cursor-pointer">
+                    {user.name}
+                </div>
+            </div>
+        );
+    });
 
+    return <div>{renderedUsers}</div>;
 }
 
-export default UsersList
+export default UsersList;
